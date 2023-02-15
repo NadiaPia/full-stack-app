caef6cc8132b1724fba78117ec2457921ab9af51
NadiaPia <206i417@gmail.com>
2023-02-15 14:02:57 -0800

    add new comment functionality in Post page

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -10,7 +10,9 @@ import axios from 'axios';
 function Post() {
 
     let { id } = useParams();  //now the program knows what to get as useParams
-    const [postObject, setPostObject] = useState({}) 
+    const [postObject, setPostObject] = useState({});
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState([]);
     
     
     useEffect(() => {
@@ -18,8 +20,23 @@ function Post() {
             setPostObject(response.data)       
             
         }) 
+
+        axios.get(`http://localhost:3001/comments/${id}`).then((response) => {  //we need keep ${id} as we need our comment appear in the same page as the post
+        setComments(response.data)
+            
+        }) 
     
       }, []);
+
+      const addComment = () => {
+        axios.post("http://localhost:3001/comments", {commentBody: newComment, PostId: id}).then((response) => {
+            const commentToAdd = { commentBody: newComment }; //each comment is an object, containing commentBody and a PostId, and here, we need only commentBody
+            setComments([...comments, commentToAdd]); //it makes a new post added automatically, but not after refresh
+            setNewComment("")
+
+        })
+      }
+     
       
   return (
     <div className="postPage" >
@@ -30,7 +47,28 @@ function Post() {
                 <div className="footer">{postObject.userNAme}</div>
             </div>
        </div>
-       <div className="rightSide">Comment Section</div>
+       <div className="rightSide">
+        <div className="addCommentContainer">
+            <input 
+                type='text' 
+                placeholder='Comment...' 
+                autoComplete='off'
+                value={newComment} //to make an input field clean after adding a comment
+                onChange={(event) => {
+                    setNewComment(event.target.value)
+                }}
+            />
+            <button onClick={addComment}>Add Comment</button>
+        </div>
+
+        <div className="listOfComments">
+            {comments.map((comment, key) => {
+                return <div key={key} className="comment">{comment.commentBody}</div>
+                
+            })}
+        </div>
+
+       </div>
     </div>
   )
 }
