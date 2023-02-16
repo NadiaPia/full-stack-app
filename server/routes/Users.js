6ae0a9832e17fa5fc77bb38b738433d7ef7859a0
NadiaPia <206i417@gmail.com>
2023-02-15 19:43:47 -0800

    Add posr request fot the regisration and post request for the login

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const { Users } = require("../models")
+const { Users } = require("../models");
+const bcrypt = require('bcrypt');
 
-
-//in this case we need a post request that will insert data in a Users table
+//Registration. Here we need grab the req.body.username and req.body.password from the 
+//request and put thm in the db
 
 router.post("/", async (req, res) => {
-    
+    const {username, password} = req.body //the same as const .......password = req.body.password.....
+    bcrypt.hash(password, 10).then((hash) => {
+        Users.create({                   //here we say our models to add new user to our db
+            userNAme: username,
+            password: hash,             //because we alredy hashed our password
+        })
+        res.json("Success")
+    })
 });
+
+// Login. Here we need grab the req.body.username and req.body.password from the 
+//request and check whether we have therm in our database
+
+router.post("/login", async(req, res) => {
+    const {username, password} = req.body //the same as const .......password = req.body.password.....
+    const user = await Users.findOne({where: {userNAme: username}});
+    if(!user) res.json({error: "User doesn't Exist"}); //if user exist, let's check the password:
+
+    //we cannot unhash user.password(password from the db,
+    //we only can hash password from the request(req.body.password) and compare them)
+    bcrypt.compare(password, user.password).then((match) => {
+        if(!match) res.json({error: "Wrong Username and Password combination"});
+        res.json("YOU LOGGED IN!!!")
+    }) 
+    
+
+})
 
 
 module.exports = router;
