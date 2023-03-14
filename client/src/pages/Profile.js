11ca86e230e1319f611db941508cdb93eba780cf
NadiaPia <206i417@gmail.com>
2023-03-13 23:45:26 -0700

    Add functionality to show the userName at Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 
 function Profile() {
     let { id } = useParams();
+    const [userName, setUserName] = useState("");
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/auth/basicinfo/${id}`). then((response) => {
+            setUserName(response.data.userNAme)
+
+        })
+    }, []);
+
   return (
     <div className="profilePageContainer">
-      <div className="basicInfo"><h1>UserName: {}</h1></div>
+      <div className="basicInfo"><h1>Username: {userName}</h1></div>
       <div className="listOfPosts"></div>
 
     </div>
