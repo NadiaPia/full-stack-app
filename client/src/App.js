bb8f6cee36193ba26c320c1c1492aa8e46bb2b3a
NadiaPia <206i417@gmail.com>
2023-03-13 22:39:04 -0700

    Add Profile page, Add profile Route, add router.get handler for the Profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Post from "./pages/Post";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Registration from "./pages/Registration";
+import Profile from "./pages/Profile";
+
 import { AuthContext } from './helpers/AuthContext';
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -66,6 +68,8 @@ function App() {
             <Route path="/post/:id" exact element={<Post />} />
             <Route path="/login" exact element={<Login />} />
             <Route path="/registration" exact element={<Registration />} />
+            <Route path="/profile/:id" exact element={<Profile />} />
+
             <Route path="*" exact element={<PageNotFound />} />
           </Routes>
         </Router>
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+
+function Profile() {
+    let { id } = useParams();
+  return (
+    <div className="profilePageContainer">
+      <div className="basicInfo"><h1>UserName: {}</h1></div>
+      <div className="listOfPosts"></div>
+
+    </div>
+  )
+}
+
+export default Profile
