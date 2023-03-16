c7b4a3cd4abb8d0000a659833fc61ead75ae1111
NadiaPia <206i417@gmail.com>
2023-03-15 22:11:57 -0700

    Add button to change the password to the Profile.js and add authState to the button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { AuthContext } from '../helpers/AuthContext';
+
 
 
 function Profile() {
@@ -8,6 +10,8 @@ function Profile() {
     let navigate = useNavigate();
     const [userName, setUserName] = useState("");
     const [listOfPosts, setListOfPosts] = useState([]);
+    const {authState} = useContext(AuthContext)            //don't foget to import useContext from 'react'
+
 
 
     useEffect(() => {
@@ -23,7 +27,11 @@ function Profile() {
 
   return (
     <div className="profilePageContainer">
-      <div className="basicInfo"><h1>Username: {userName}</h1></div>
+      <div className="basicInfo">
+        <h1>Username: {userName}</h1>
+        {authState.userName === userName && <button>Change My Password</button>}
+      </div>
+
       <div className="listOfPosts">
       {listOfPosts.map((value, key) => {
       return (
